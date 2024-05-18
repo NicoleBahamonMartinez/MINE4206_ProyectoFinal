6540406d3d155318f6d136cb6ce981d9b4f21b80
Nicole Bahamón Martínez <n.bahamon@uniandes.edu.co>
2024-05-18 09:15:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
-st.title("Juli me ama ")
+st.title("Proyecto Final: Análisis con Machine Learning")
+
+st.subheader("Usabilidad Datos Abiertos")
+
+st.write('Por favor ingrese las métricas de su dataset')
+
+st.text_input("Titulo")
